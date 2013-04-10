ed5a9d9af0a062a90a0b5e75665556328b21fd20
EAK <ashok.zg@gmail.com>
2013-04-10 18:34:16 -0400

    Teleop working

--- eod_teleop/src/eod_teleop.py
+++ eod_teleop/src/eod_teleop.py
@@ -8,37 +8,39 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Int32
 
 global manual
-manual = False
+manual = [False]
 cmd_vel = Velocity()
 cmd_vel.linVelPcent = 0
 cmd_vel.angVelPcent = 0
-count = 0
+count = [0]
 
 def joy_callback(data):
     cmd_vel.linVelPcent = 0
     cmd_vel.angVelPcent = 0
-    if manual == True:
-        count = 0
+    if manual[0] == True:
+        print "manual"
+        count[0] = 0
         #Only if dead man's button is pressed take any action
         if data.buttons[4] == 1:
-            cmd_vel.linVelPcent = -data.axes[1]
-            cmd_vel.angVelPcent = -data.axes[0]/2
+            cmd_vel.linVelPcent = data.axes[1]
+            cmd_vel.angVelPcent = data.axes[0]/2
         robotCmdPub.publish(cmd_vel)
     #If RED button and the deadman's switch is pressed, stop the robot
     if data.buttons[1] == 1 and data.buttons[4] == 1:
-        count += 1
+        count[0] += 1
         robotCmdPub.publish(cmd_vel)
-        if count == 10:
-            manual = True
+        if count[0] == 10:
+            manual[0] = True
 
 def uiState(data):
     if data.data == 1:
-        manual = True
+        manual[0] = True
     else:
-        manual = False
+        manual[0] = False
 
 rospy.init_node("eod_teleop")
 rospy.Subscriber("joy", Joy, joy_callback)
 rospy.Subscriber("UiStatus", Int32, uiState)
-robotCmdPub = rospy.Publisher('cmd_vel', Velocity)    
+robotCmdPub = rospy.Publisher('cmd_vel', Velocity)
+print "EOD Teleop started. Start joystick"    
 rospy.spin()
