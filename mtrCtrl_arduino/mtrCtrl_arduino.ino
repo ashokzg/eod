518ff6db2caee0c0424fa4e599880e19465e358e
Ashok <ashok.zg@gmail.com>
2013-04-11 07:35:11 -0400

    3-ultrasound with custom msg

--- mtrCtrl_arduino/mtrCtrl_arduino.ino
+++ mtrCtrl_arduino/mtrCtrl_arduino.ino
@@ -15,14 +15,34 @@
 
 #include <ros.h>
 #include <ros/time.h>
-#include <sensor_msgs/Range.h>
 #include <vel_msgs/Velocity.h>
+#include <ultrasonic/Ultrasonic.h>
 #include "DualVNH5019MotorShield.h"
 //Full Speed = 400 : 0-255 for Analog Write mapped to 400
+
 #define FS 400
-#define ULTRA_PIN A0
+#define ULTRA_FRONT A0
+#define ULTRA_RIGHT A1
+#define ULTRA_LEFT A2
+#define ENC_LEFT 2
+#define ENC_RIGHT 3
 
+#define Circum 35.0 //circumference of wheel, length per rev
+#define CPR 80 // Counts Per Revolution
+#define TCC 24 //Turning circle circumference in cm
 
+// Volatile variables that can be changed inside of interrupt functions
+unsigned int RightEncoderPos;
+unsigned int LeftEncoderPos;
+unsigned int Lcount,Rcount;
+unsigned long LdVal = 0;
+unsigned long RdVal = 0;
+
+DualVNH5019MotorShield md;
+
+ros::NodeHandle  nh;
+ultrasonic::Ultrasonic us_msg;
+ros::Publisher pub_range( "/ultrasound", &us_msg);
 const int adc_pin = 0;
 
 char frameid[] = "/ultrasound";
@@ -78,35 +98,15 @@ void messageCb( const vel_msgs::Velocity& vel){
   md.setM2Speed(leftVel);
 }
 
-
+ros::Subscriber<vel_msgs::Velocity> sub("cmd_vel", &messageCb );  
 
 void setup()
 { 
   
-  
-DualVNH5019MotorShield md;
-
-ros::NodeHandle  nh;
-sensor_msgs::Range range_msg;
-ros::Publisher pub_range( "/ultrasound", &range_msg);
-ros::Subscriber<vel_msgs::Velocity> sub("cmd_vel", &messageCb );  
-
-
   md.init();
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(pub_range);
-
-
-  
-  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
-  //range_msg.header.frame_id =  frameid;
-  //range_msg.field_of_view = 0.1;  // fake
-  range_msg.min_range = 0.0;
-  range_msg.max_range = 6.47;
-  
-  pinMode(8,OUTPUT);
-  digitalWrite(8, LOW);
 }
 
 long range_time;
@@ -116,12 +116,13 @@ void loop()
 
   //publish the adc value every 50 milliseconds
   //since it takes that long for the sensor to stablize
+  
   if ( millis() >= range_time ){
-    int r =0;
-
-    range_msg.range = getRange_Ultrasound(ULTRA_PIN);
-    range_msg.header.stamp = nh.now();
-    pub_range.publish(&range_msg);
+    us_msg.ultra_centre = getRange_Ultrasound(ULTRA_FRONT);
+    us_msg.ultra_right = getRange_Ultrasound(ULTRA_RIGHT);
+    us_msg.ultra_left = getRange_Ultrasound(ULTRA_LEFT);    
+    us_msg.header.stamp = nh.now();
+    pub_range.publish(&us_msg);
     range_time =  millis() + 50;
   }
 
