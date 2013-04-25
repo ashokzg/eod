18d8eadcac853934faf95d450c83a5c09fea5aa5
Anusha <anusha@anusha-hiccup.(none)>
2013-04-25 05:47:40 -0400

    Imported UInt32

--- differential_drive/nodes/diff_tf.py
+++ differential_drive/nodes/diff_tf.py
@@ -60,7 +60,7 @@ from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from tf.broadcaster import TransformBroadcaster
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, UInt32
 
 #############################################################################
 class DiffTf:
