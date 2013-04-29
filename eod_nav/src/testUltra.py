78a4d5e82421e161528b8165df3bb148ab16afe3
Ashok <ashok.zg@gmail.com>
2013-04-29 12:53:12 -0400

    Used arduino mega drivers, test servo

--- eod_nav/src/testUltra.py
+++ eod_nav/src/testUltra.py
@@ -4,21 +4,31 @@ import roslib; roslib.load_manifest('eod_nav')
 import numpy
 from sensor_msgs.msg import CameraInfo, Range
 from eod_nav.msg import Ultrasonic
+from std_msgs.msg import UInt32
 
 count = 0
 
 class temp:
   val = numpy.zeros(15)  
   count = 0
+  TIME_STEP = 50
+  totalSweepStates = 25
+  servoAngle = UInt32()
+  servoPub = rospy.Publisher('servo_angle', UInt32)
+  sweepDist = [601]*totalSweepStates
+  sweepcall = False
+  dist = [600]*3
+  OBS_AVOID_DIST = 300
   def ultrasound(self, data):
     self.count += 1    
-    self.val = numpy.append(self.val, data.ultra_left)
+    self.val = numpy.append(self.val, data.ultra_centre)
     self.val = numpy.delete(self.val, 1)  
     #print val
     if self.count > 11:
       v = self.smooth(self.val)
       #print v
-      print v.mean()
+      #print v.mean()
+      self.dist[1] = v.mean()
 
   def smooth(self, x, window_len=11,window='hanning'):
     """smooth the data using a window with requested size.
@@ -77,8 +87,65 @@ class temp:
     y=numpy.convolve(w/w.sum(),s,mode='valid')
     return y
   
+  def sweepcb(self, data):
+    self.sweepState = 0
+    self.sweepCount = 0
+    self.sweepSet = False
+    self.sweepcall = True
+    
+  def sweep(self):
+    if self.sweepState <= self.totalSweepStates:
+      if self.sweepSet == False:
+        self.servoAngle.data = (self.sweepState-((self.totalSweepStates-1)/2))*5 + 90              
+        self.servoPub.publish(self.servoAngle)
+        self.sweepSet = True
+        self.sweepCount = 0
+      elif self.sweepCount*self.TIME_STEP > 700:        
+        self.sweepSet = False
+        if self.sweepState < self.totalSweepStates:
+          print self.servoAngle.data, self.dist[1]
+          self.sweepDist[self.sweepState] = self.dist[1]
+        self.sweepState += 1
+        if self.sweepState == self.totalSweepStates:
+          #Terminal case
+          self.servoAngle.data = 90
+          self.servoPub.publish(self.servoAngle)         
+    self.sweepCount +=  1
+  
+  def handler(self):
+    if self.sweepcall == True:
+      self.sweep()
+      self.rightObsCount = 0
+      self.leftObsCount = 0
+      midP = (self.totalSweepStates-1)/2
+      if self.sweepState == self.totalSweepStates :
+        self.sweepcall = False
+        self.swept = True
+        for i in range(0,midP):
+          if self.sweepDist[i] < self.OBS_AVOID_DIST:
+            self.rightObsCount += 1
+          if self.sweepDist[i+midP+1] < self.OBS_AVOID_DIST:
+            self.leftObsCount += 1
+        if self.leftObsCount > self.rightObsCount:
+          self.avoidDirection = self.RIGHT
+          leftLimit = 40
+          rightLimit = 120
+        elif self.leftObsCount == self.rightObsCount:
+          if self.dest.destPresent == True:
+            cx = self.dest.destX + self.dest.destWidth/2
+            if cx < self.imgWidth/2:
+              pass
+        else:
+          self.avoidDirection = self.LEFT
+          leftLimit = self.imgWidth - 120
+          rightLimit = self.imgWidth - 40      
+  
 if __name__ == "__main__":  
   rospy.init_node("testultra")  
   a = temp()
   rospy.Subscriber("ultrasound", Ultrasonic, a.ultrasound)
-  rospy.spin()
+  rospy.Subscriber("sweep", UInt32, a.sweepcb)
+  r = rospy.Rate(20) #Run every 50 ms
+  while not rospy.is_shutdown():
+    a.handler()
+    r.sleep()
